95a61d51c5f71cab0b47c921391580fe699cbcc6
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-17 11:39:53 +0530

    frame using index or string

--- src/com/johndeere/frame/Demo3.java
+++ src/com/johndeere/frame/Demo3.java
@@ -5,8 +5,8 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//using webelement
-public class Demo1 {
+//using index
+public class Demo3 {
 
 	public static void main(String[] args) {
 		
@@ -18,7 +18,7 @@ public class Demo1 {
 		
 		driver.get("https://netbanking.hdfcbank.com/netbanking/");
 		
-		driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='login_page']")));
+		driver.switchTo().frame(0);
 		
 		driver.findElement(By.name("fldLoginUserId")).sendKeys("test123");
 		driver.findElement(By.linkText("CONTINUE")).click();
