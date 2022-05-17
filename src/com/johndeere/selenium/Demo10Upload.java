8dddccc6aac0437fa5440d3777cc7baf88a61834
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-05-17 16:08:04 +0530

    upload

--- src/com/johndeere/selenium/Demo10Upload.java
+++ src/com/johndeere/selenium/Demo10Upload.java
@@ -1,18 +1,11 @@
 package com.johndeere.selenium;
 
-import java.io.File;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+	
 public class Demo10Upload {
 
 	public static void main(String[] args) {
@@ -27,7 +20,7 @@ public class Demo10Upload {
 
 		driver.findElement(By.xpath("//input[@type='file']")).sendKeys("D:\\Balaji-Profile.pdf");
 		
-
+		
 	}
 }
 
